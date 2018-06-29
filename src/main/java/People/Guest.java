105cf0898c10d47410f995a3cc47bc8f775d0b7f
StuartCharters <charters.stuart@gmail.com>
2018-06-29 11:50:52 +0100

    tested all functions of a Room for a Bedroom and passed

--- src/main/java/People/Guest.java
+++ src/main/java/People/Guest.java
@@ -1,4 +1,10 @@
 package People;
 
 public class Guest {
+
+    private double wallet;
+
+    public Guest(double wallet){
+        this.wallet = wallet;
+    }
 }
--- src/test/java/Hotel/RoomTypes/BedroomTest.java
+++ src/test/java/Hotel/RoomTypes/BedroomTest.java
@@ -1,5 +1,6 @@
 package Hotel.RoomTypes;
 
+import People.Guest;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -9,15 +10,35 @@ public class BedroomTest {
 
     private Bedroom bedroom1;
     private Bedroom bedroom2;
+    private Guest guest;
 
     @Before
     public void before(){
         bedroom1 = new Bedroom(BedroomTypes.SINGLE, 1);
         bedroom2 = new Bedroom(BedroomTypes.DOUBLE, 2);
+        guest = new Guest(200.00);
+
     }
 
     @Test
     public void bedroomStartsEmpty(){
         assertEquals(0, bedroom1.getNumberOfOccupants());
+    }
+
+    @Test
+    public void canGetCapacity(){
+        assertEquals(1, bedroom1.getCapacity());
+    }
+
+    @Test
+    public void canAddGuest(){
+        bedroom1.addOccupant(guest);
+        assertEquals(1, bedroom1.getNumberOfOccupants());
+    }
+
+    @Test
+    public void canGetArrayListOfOccupants(){
+        bedroom1.addOccupant(guest);
+        assertEquals(guest, bedroom1.getOccupants().get(0));
     }
 }
